ff536471b41502fa4018131d154bf5393de8490e
goater <goater1095@gmail.com>
2020-12-09 23:54:27 +0800

    add user page & user routes

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const home = require('./modules/home');
 const sort = require('./modules/sort');
 const records = require('./modules/records');
+const users = require('./modules/users');
 
 router.use('/', home);
 router.use('/sort', sort);
 router.use('/records', records);
+router.use('/users', users);
 
 module.exports = router;
